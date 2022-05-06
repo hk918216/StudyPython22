9a48984767488b3a1abc1a12c70b94dc0cb9e02c
hk918216 <hk981216@gmail.com>
2022-05-06 17:42:54 +0900

    220506 주소록 최종 및 객제지향

--- advanced/address_book.py
+++ advanced/address_book.py
@@ -25,38 +25,50 @@ class Contact:
 
 def run():
     lst_contact = []     # 빈리스트 생성
+    load_contact(lst_contact)  #파일DB 읽어오기
     clearConsole()
     while True:
-        sel_menu= get_menu()
+        sel_menu = get_menu()
         if sel_menu == 1:
             clearConsole()
             contact = set_contact()
+            if contact is None:
+                input('계속하려면 엔터를 누르세요')
+                clearConsole()
+                continue  #contact가 비면 리스트 추가불가
+
             lst_contact.append(contact)
-            input()   #아무값도 안받지만 엔터 대기
+            input('연락처 추가 성공 \n 계속하려면 엔터를 누르세요')   #아무값도 안받지만 엔터 대기
             clearConsole()
         elif sel_menu == 2: #연락처 출력
             clearConsole()
             get_contact(lst_contact)
-            input()   #아무값도 안받지만 엔터 대기
+            input('계속하려면 엔터를 누르세요')   #아무값도 안받지만 엔터 대기
             clearConsole()
         elif sel_menu ==3 :
             clearConsole()
             name = input('삭제할 이름 입력 > ')
             del_contact(lst_contact, name)
-            input()
+            input('연락처 삭제 성공 \n 계속하려면 엔터를 누르세요')
             clearConsole()
         elif sel_menu == 4:
+            save_contact(lst_contact) # 파일DB저장
             break
         else:
-            clearConsole(())
+            clearConsole()
         
 
 # 주소록 입력함수
 def set_contact():
-    name, phone_num, e_mail, addr = \
+    contact = None
+    try:  #아무입력없이 엔터1 | 갯수 틀리면 생기는 예외처리
+        name, phone_num, e_mail, addr = \
         input('정보입력(이름, 핸드폰, 이메일, 주소(구분자 /))').split('/')
-    contact = Contact(phone_num=phone_num, name=name, e_mail=e_mail, addr=addr)
-    return contact
+        contact = Contact(phone_num=phone_num, name=name, e_mail=e_mail, addr=addr)
+    except Exception as e:
+        print('입력갯수 확인(이름/핸드폰/이메일/주소)')
+    finally:
+        return contact #잘못되면 None 리턴, Contact 객체 리턴
 
 # 주소록 전체출력
 def get_contact(lst_contact):
@@ -69,6 +81,33 @@ def del_contact(lst_contact, name):
         if contact.name == name:
             del lst_contact[i]
 
+# 주소록 파일DB 저장
+def save_contact(lst_contact):
+    f = open('./advanced/db_contact.txt', mode='w', encoding='utf-8')
+    for contact in lst_contact:
+        f.write(contact.name + '/') 
+        f.write(contact.phone_num + '/')
+        f.write(contact.e_mail + '/')
+        f.write(contact.addr + '\n')
+
+    f.close()
+ 
+# 주소록 파일DB 읽어오기
+def load_contact(lst_contact:list):
+    f = open('./advanced/db_contact.txt', mode='r', encoding='utf-8')
+    while True:
+        line = f.readline()
+        if not line: break
+
+        lines = line.rstrip('\n').split('/')  # 줄마지막 \n 제거하고, /로 나누고 나눈값을 리스트로
+        if len(lines) != 4: continue   # 사이에 엔터공백있어도 출력 가능. 220506 엔터로 생기는
+        contact = Contact(lines[0], lines[1],  lines[2],  lines[3])
+        lst_contact.append(contact)
+
+    f.close()
+
+
+# 메뉴 출력 및 선택
 def get_menu():
     str_menu = ('--주소록 프로그램 ver.1.1--\n'
                 '1. 연락처 추가\n'
@@ -76,8 +115,14 @@ def get_menu():
                 '3. 연락처 삭제\n'
                 '4. 종료\n')
     print(str_menu)
-    menu = input('메뉴입력 : ')
-    return int(menu)
+    #0~9 숫자 외 ValueError 발생
+    menu = 0   #초기화
+    try:
+        menu = int(input('메뉴입력 : '))
+    except Exception as e:
+        print(e)
+    finally:
+        return menu
 
 def clearConsole() :
     command = 'clear' #mac,linux, unix 화면 클리어 명령어
@@ -86,4 +131,8 @@ def clearConsole() :
     os.system(command)
 
 if __name__ == '__main__' :
-    run()
+    try:
+        run()
+    except KeyboardInterrupt as e:
+        print('Ctrl+c 종료')
+    
---
+++ advanced/class02.py
@@ -0,0 +1,39 @@
+# 객체지향 - 클래스 상속
+class Vehicle:
+    name = '탈것'
+    color = '색상'
+
+    def __init__(self, color = None) -> None:
+        self.color = color
+        print(f'{self.color}색 {self.name} 생성!')
+
+    def desc(self):
+        print(f'{self.name} 객체')
+    
+    def move(self):
+        print(f'{self.name} 이동!')
+
+class Car(Vehicle): #Vehicle 클래스를 상속받은 Car
+    name = '자동차'
+    brand = '현대'
+
+    def __init__(self, color = None) -> None:
+        super().__init__(color)
+        print(f'{color}색 {self.brand} {self.name} 생성!!')
+
+    def move(self):
+        # print(f'{self.name} 달린다!')
+        super().move()   #부모가 가진 move를 쓰겠다. => '자동차 이동'으로 출력
+
+    def stop(self):
+        print('브레이크로 멈춤')
+
+if __name__ == '__main__':
+    vcl = Vehicle('검은')
+    vcl.desc()
+    vcl.move()
+
+    mycar = Car('흰')
+    mycar.desc()
+    mycar.move()
+    mycar.stop()
